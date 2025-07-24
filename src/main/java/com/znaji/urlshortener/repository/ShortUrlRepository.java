264d1a1672f83c737951a3f6de4c3acac22bfcad
ZakaNaji <zakaria.naji@edu.uca.ma>
2025-07-24 12:54:54 +0100

    #3: - avoid n+1 query problem - disable open-in-view jpa property => so that jpa doesn't keep session open for the entirity of the request. - change our jpql query to eagarly fetch the user

--- src/main/java/com/znaji/urlshortener/repository/ShortUrlRepository.java
+++ src/main/java/com/znaji/urlshortener/repository/ShortUrlRepository.java
@@ -1,12 +1,15 @@
 package com.znaji.urlshortener.repository;
 
 import com.znaji.urlshortener.domain.entity.ShortUrl;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
-    @Query("select su from ShortUrl as su where su.isPrivate = false order by su.createdAt desc ")
+    //@Query("select su from ShortUrl as su JOIN FETCH su.createdBy where su.isPrivate = false order by su.createdAt desc")
+    @Query("select su from ShortUrl su where su.isPrivate = false order by su.createdAt desc ")
+    @EntityGraph(attributePaths = "createdBy")
     List<ShortUrl> findAllPublicUrls();
 }
